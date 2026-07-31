b3a336bf58ad926bb7c42ec57f151c611d21331d
Jayashri N <jayashrin1403@gmail.com>
2026-07-31 23:17:50 +0530

    Fix Vercel Express deployment

--- server.js
+++ server.js
@@ -209,6 +209,10 @@ app.delete("/api/tasks/:id", authenticate, async (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+if (require.main === module) {
+  app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+  });
+}
+
+module.exports = app;
